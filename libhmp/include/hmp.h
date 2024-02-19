366f29ff65bdca976ae357c61f2ce412fe9bf673
Kai Uerlichs <kai@uerlichs.de>
2024-02-19 12:06:21 +0000

    HON-15 Refactor to header-only for template meta programming

--- libhmp/include/hmp.h
+++ libhmp/include/hmp.h
@@ -1,10 +1,14 @@
 #ifndef INCLUDE_HMP_H_
 #define INCLUDE_HMP_H_
 
+#include <cstddef>
+#include <cstdio>
 #include <memory>
 #include <vector>
 
 #include "mpi.h"
+#include "omp.h"
+
 namespace hmp {
 
 // Stores information about a given node in an MPI cluster
@@ -75,6 +79,118 @@ public:
   std::vector<std::pair<int, int>> get_thread_distribution();
   int get_rank();
 };
+
+// IMPLEMENTATION
+
+inline NodeInfo::NodeInfo() {
+  load_rank();
+  load_thread_count();
+  load_processor_name();
+}
+
+inline void NodeInfo::load_rank() {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  process_rank = rank;
+}
+
+inline void NodeInfo::load_thread_count() { thread_count = omp_get_max_threads(); }
+
+inline void NodeInfo::load_processor_name() {
+  int name_length;
+  MPI_Get_processor_name(processor_name, &name_length);
+}
+
+inline void NodeInfo::print_info() {
+  std::printf("Node information for %s: Rank %i, Threads %i\n", processor_name,
+              process_rank, thread_count);
+}
+
+inline bool NodeInfo::is_master() { return process_rank == 0; }
+
+inline int NodeInfo::get_rank() { return process_rank; }
+
+inline int NodeInfo::get_thread_count() { return thread_count; }
+
+inline MPI_Datatype NodeInfo::get_mpi_type() {
+  int blocklengths[3] = {1, 1, MPI_MAX_PROCESSOR_NAME};
+
+  MPI_Aint displacements[3];
+  displacements[0] = offsetof(NodeInfo, process_rank);
+  displacements[1] = offsetof(NodeInfo, thread_count);
+  displacements[2] = offsetof(NodeInfo, processor_name);
+
+  MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_CHAR};
+  MPI_Datatype mpi_node_info_type;
+  MPI_Type_create_struct(3, blocklengths, displacements, types,
+                         &mpi_node_info_type);
+  MPI_Type_commit(&mpi_node_info_type);
+  return mpi_node_info_type;
+}
+
+inline WorldInfo::WorldInfo() { MPI_Comm_size(MPI_COMM_WORLD, &node_count); }
+
+inline void WorldInfo::add_node(std::shared_ptr<NodeInfo> node_ptr) {
+  thread_count += node_ptr->get_thread_count();
+  nodes.push_back(node_ptr);
+}
+
+inline int WorldInfo::get_node_count() { return node_count; }
+
+inline void WorldInfo::print_info() {
+  for (auto node_info : nodes) {
+    node_info->print_info();
+  }
+}
+
+inline int WorldInfo::get_thread_count() { return thread_count; }
+
+inline std::vector<std::pair<int, int>> WorldInfo::get_thread_distribution() {
+  std::vector<std::pair<int, int>> distribution;
+  for (auto node_info : nodes) {
+    distribution.push_back(
+        {node_info->get_rank(), node_info->get_thread_count()});
+  }
+  return distribution;
+}
+
+inline MPICluster::MPICluster() {
+  MPI_Init(NULL, NULL);
+  self = std::make_shared<NodeInfo>();
+  world = std::make_shared<WorldInfo>();
+
+  MPI_Datatype mpi_node_info_type = self->get_mpi_type();
+
+  if (self->is_master()) {
+    world->add_node(self);
+    for (int i = 1; i < world->get_node_count(); ++i) {
+      NodeInfo node_info;
+      MPI_Recv(&node_info, 1, mpi_node_info_type, i, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      world->add_node(std::make_shared<NodeInfo>(node_info));
+    }
+    world->print_info();
+  } else {
+    MPI_Send(self.get(), 1, mpi_node_info_type, 0, 0, MPI_COMM_WORLD);
+  }
+}
+
+inline bool MPICluster::on_master() { return self->is_master(); }
+
+inline int MPICluster::get_node_count() { return world->get_node_count(); }
+
+inline int MPICluster::get_total_thread_count() { return world->get_thread_count(); }
+
+inline int MPICluster::get_local_thread_count() { return self->get_thread_count(); }
+
+inline int MPICluster::get_rank() { return self->get_rank(); }
+
+inline std::vector<std::pair<int, int>> MPICluster::get_thread_distribution() {
+  return world->get_thread_distribution();
+}
+
+inline MPICluster::~MPICluster() { MPI_Finalize(); }
+
 } // namespace hmp
 
 #endif // INCLUDE_HMP_H_
--- libhmp/include/hmp/map.h
+++ libhmp/include/hmp/map.h
@@ -1,12 +1,14 @@
-#ifndef INCLUDE_HMP_MAP_H_
-#define INCLUDE_HMP_MAP_H_
+#ifndef HMP_MAP_H_
+#define HMP_MAP_H_
 
 #include <functional>
 #include <memory>
-#include <sys/wait.h>
 #include <vector>
 
 #include "hmp.h"
+#include "hmp/typeutil.h"
+
+#include "mpi.h"
 
 namespace hmp {
 
@@ -36,6 +38,93 @@ public:
                                 std::function<OUT_TYPE(IN_TYPE)> map_function);
 };
 
+// IMPLEMENTATION
+
+template <typename IN_TYPE, typename OUT_TYPE>
+Map<IN_TYPE, OUT_TYPE>::Map(std::shared_ptr<MPICluster> cluster_ptr) {
+  cluster = cluster_ptr;
+}
+
+template <typename IN_TYPE, typename OUT_TYPE>
+std::vector<OUT_TYPE>
+Map<IN_TYPE, OUT_TYPE>::execute(std::vector<IN_TYPE> &data,
+                                std::function<OUT_TYPE(IN_TYPE)> map_function) {
+  prepare_data(data);
+  run_map_function(map_function);
+  gather_data();
+
+  return return_data;
+}
+
+// Creates buffers for local and master data, and calculates elements
+// and displacements per node
+template <typename IN_TYPE, typename OUT_TYPE>
+void Map<IN_TYPE, OUT_TYPE>::prepare_data(std::vector<IN_TYPE> &data) {
+  elements_per_node.resize(cluster->get_node_count());
+
+  if (cluster->on_master()) {
+    std::vector<std::pair<int, int>> thread_distribution =
+        cluster->get_thread_distribution();
+    int total_threads = cluster->get_total_thread_count();
+
+    int data_elements = data.size();
+    int elements_per_thread = (int)data_elements / total_threads;
+    int remaining_elements = data_elements % total_threads;
+    
+    // Calculates block wise distribution per node
+    for (auto node_threads : thread_distribution) {
+      auto [rank, threads] = node_threads;
+      int extra_elements =
+          remaining_elements < threads ? remaining_elements : threads;
+      elements_per_node[rank] = elements_per_thread * threads + extra_elements;
+      remaining_elements -= extra_elements;
+    }
+
+    return_data.resize(data_elements);
+  }
+
+  // Broadcast and creates local buffers
+  MPI_Bcast(elements_per_node.data(), elements_per_node.size(), MPI_INT, 0,
+            MPI_COMM_WORLD);
+  local_data.resize(elements_per_node[cluster->get_rank()]);
+  local_return_data.resize(elements_per_node[cluster->get_rank()]);
+  
+  // TODO: Is this more efficient to calculate on master and bcast?
+  // Calculate displacement of blocks per node
+  displacements_per_node.resize(elements_per_node.size());
+  int cumulative_sum = 0;
+  for (int i = 0; i < elements_per_node.size(); ++i) {
+    displacements_per_node[i] = cumulative_sum;
+    cumulative_sum += elements_per_node[i];
+  }
+  
+  // Distribute data over nodes into local buffers
+  MPI_Scatterv(data.data(), elements_per_node.data(),
+               displacements_per_node.data(), hmputils::get_mpi_type<IN_TYPE>(),
+               local_data.data(), elements_per_node[cluster->get_rank()],
+               hmputils::get_mpi_type<IN_TYPE>(), 0, MPI_COMM_WORLD);
+}
+
+template <typename IN_TYPE, typename OUT_TYPE>
+void Map<IN_TYPE, OUT_TYPE>::run_map_function(
+    std::function<OUT_TYPE(IN_TYPE)> map_function) {
+  // TODO: Implement thread-level parallelism around map function
+  //  Use pthreads or OMP?
+  // Runs map function through a for-loop
+  for (int i = 0; i < local_data.size(); ++i) {
+    local_return_data[i] = map_function(local_data[i]);
+  }
+}
+
+template <typename IN_TYPE, typename OUT_TYPE>
+void Map<IN_TYPE, OUT_TYPE>::gather_data() {
+  // Gather data to master buffer
+  MPI_Gatherv(local_return_data.data(), elements_per_node[cluster->get_rank()],
+              hmputils::get_mpi_type<OUT_TYPE>(), return_data.data(),
+              elements_per_node.data(), displacements_per_node.data(),
+              hmputils::get_mpi_type<OUT_TYPE>(), 0, MPI_COMM_WORLD);
+}
+
 } // namespace hmp
 
-#endif // INCLUDE_HMP_MAP_H_
+#endif // HMP_MAP_H_
