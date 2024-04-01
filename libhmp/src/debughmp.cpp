ffc0f7764fd48d4779c1eb5f1dde312c412fe486
Kai Uerlichs <kai@uerlichs.de>
2024-04-01 23:36:04 +0100

    Update debughmp program

--- libhmp/src/debughmp.cpp
+++ libhmp/src/debughmp.cpp
@@ -10,33 +10,40 @@
 
 int test(int i) { return i * i; }
 
-int main() {
-  std::shared_ptr<hmp::MPICluster> cluster =
-      std::make_shared<hmp::MPICluster>();
-  std::unique_ptr<hmp::Map<int, int>> map =
-      std::make_unique<hmp::Map<int, int>>(cluster, hmp::Distribution::CORE_FREQUENCY);
+std::vector<int> generate_test_data() {
   std::vector<int> vector;
-
-  if (cluster->on_master()) {
-    for (int i = 0; i < 64; ++i) {
-      vector.push_back(i);
-    }
-    printf("Input data: ");
-    for (auto i : vector) {
-      printf("%i ", i);
-    }
-    printf("\n");
+  for (int i = 0; i < 64; ++i) {
+    vector.push_back(i);
   }
-
-  std::vector<int> ret = map->execute(vector, test);
-
-  if (cluster->on_master()) {
-    printf("Output data: ");
-    for (auto i : ret) {
-      printf("%i ", i);
-    }
-    printf("\n");
+  printf("Input data: ");
+  for (auto i : vector) {
+    printf("%i ", i);
   }
+  printf("\n");
+  return vector;
+}
+
+void handle_output_data(std::vector<int> data) {
+  printf("Output data: ");
+  for (auto i : data) {
+    printf("%i ", i);
+  }
+  printf("\n");
+}
+
+int main() {
+  auto cluster = std::make_shared<hmp::MPICluster>();
+  std::vector<int> data;
+
+  if (cluster->on_master())
+    data = generate_test_data();
+
+  auto map = std::make_unique<hmp::Map<int, int>>(
+      cluster, hmp::Distribution::CORE_FREQUENCY);
+  std::vector<int> return_data = map->execute(data, test);
+
+  if (cluster->on_master())
+    handle_output_data(return_data);
 
   return 0;
 }
