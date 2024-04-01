4d11169518466fb2ede1c36741c0e2fb49f6d9eb
Kai Uerlichs <kai@uerlichs.de>
2024-04-01 18:00:56 +0100

    refactor base hmp structure for clarity

--- libhmp/include/hmp.h
+++ libhmp/include/hmp.h
@@ -12,22 +12,25 @@
 namespace hmp {
 
 // Stores information about a given node in an MPI cluster
-class NodeInfo {
+class Node {
 private:
   int process_rank;
-  int thread_count;
+  int processor_frequency;
   char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int thread_count;
+  bool os_linux;
 
   void load_rank();
-  void load_thread_count();
-  void load_processor_name();
+  void load_os_type();
+  void load_processor_info();
 
 public:
-  NodeInfo();
-  ~NodeInfo(){};
+  Node();
+  ~Node(){};
 
   void print_info();
   bool is_master();
+  bool is_linux();
   int get_rank();
   int get_thread_count();
 
@@ -36,36 +39,19 @@ public:
 };
 
 // Stores information about the MPI cluster global context
-class WorldInfo {
-private:
-  int node_count;
-  int thread_count;
-  std::vector<std::shared_ptr<NodeInfo>> nodes;
-
-public:
-  WorldInfo();
-  ~WorldInfo(){};
-
-  void add_node(std::shared_ptr<NodeInfo>);
-  int get_node_count();
-  int get_thread_count();
-
-  // Returns a vector of integer pairs
-  //  First: The node rank
-  //  Second: The number of threads on that node
-  std::vector<std::pair<int, int>> get_thread_distribution();
-  
-  void print_info();
-};
-
-// Top level wrapper class for the MPI functionality
 class MPICluster {
 private:
-  std::shared_ptr<NodeInfo> self;
-  std::shared_ptr<WorldInfo> world;
+  std::shared_ptr<Node> self;
+  std::vector<std::shared_ptr<Node>> nodes;
 
-  void send_node_info();
-  void receive_node_info();
+  int node_count;
+  int thread_count;
+  bool os_linux;
+
+  void add_node(std::shared_ptr<Node>);
+
+  void send_node();
+  void receive_node();
 
 public:
   // Constructor/destructor
@@ -73,120 +59,135 @@ public:
   ~MPICluster();
 
   bool on_master();
+  bool is_linux();
   int get_node_count();
   int get_total_thread_count();
   int get_local_thread_count();
   std::vector<std::pair<int, int>> get_thread_distribution();
   int get_rank();
+  void print_info();
 };
 
 // IMPLEMENTATION
 
-inline NodeInfo::NodeInfo() {
+inline Node::Node() {
   load_rank();
-  load_thread_count();
-  load_processor_name();
+  load_os_type();
+  load_processor_info();
 }
 
-inline void NodeInfo::load_rank() {
+inline void Node::load_rank() {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   process_rank = rank;
 }
 
-inline void NodeInfo::load_thread_count() { thread_count = omp_get_max_threads(); }
-
-inline void NodeInfo::load_processor_name() {
+inline void Node::load_processor_info() {
   int name_length;
   MPI_Get_processor_name(processor_name, &name_length);
+
+  thread_count = omp_get_max_threads();
+
+  if (is_linux()) {
+    // TODO: Load processor frequency
+  }
 }
 
-inline void NodeInfo::print_info() {
+inline void Node::load_os_type() {
+// TODO: This implementation evaluates OS type on compile time, which might need
+// to be changed to runtime
+#ifdef __linux__
+  os_linux = true;
+#else
+  os_linux = false;
+#endif
+}
+
+inline void Node::print_info() {
   std::printf("Node information for %s: Rank %i, Threads %i\n", processor_name,
               process_rank, thread_count);
 }
 
-inline bool NodeInfo::is_master() { return process_rank == 0; }
+inline bool Node::is_master() { return process_rank == 0; }
 
-inline int NodeInfo::get_rank() { return process_rank; }
+inline bool Node::is_linux() { return os_linux; }
 
-inline int NodeInfo::get_thread_count() { return thread_count; }
+inline int Node::get_rank() { return process_rank; }
 
-inline MPI_Datatype NodeInfo::get_mpi_type() {
+inline int Node::get_thread_count() { return thread_count; }
+
+inline MPI_Datatype Node::get_mpi_type() {
   int blocklengths[3] = {1, 1, MPI_MAX_PROCESSOR_NAME};
 
   MPI_Aint displacements[3];
-  displacements[0] = offsetof(NodeInfo, process_rank);
-  displacements[1] = offsetof(NodeInfo, thread_count);
-  displacements[2] = offsetof(NodeInfo, processor_name);
+  displacements[0] = offsetof(Node, process_rank);
+  displacements[1] = offsetof(Node, thread_count);
+  displacements[2] = offsetof(Node, processor_name);
 
   MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_CHAR};
-  MPI_Datatype mpi_node_info_type;
-  MPI_Type_create_struct(3, blocklengths, displacements, types,
-                         &mpi_node_info_type);
-  MPI_Type_commit(&mpi_node_info_type);
-  return mpi_node_info_type;
-}
-
-inline WorldInfo::WorldInfo() { MPI_Comm_size(MPI_COMM_WORLD, &node_count); }
-
-inline void WorldInfo::add_node(std::shared_ptr<NodeInfo> node_ptr) {
-  thread_count += node_ptr->get_thread_count();
-  nodes.push_back(node_ptr);
-}
-
-inline int WorldInfo::get_node_count() { return node_count; }
-
-inline void WorldInfo::print_info() {
-  for (auto node_info : nodes) {
-    node_info->print_info();
-  }
-}
-
-inline int WorldInfo::get_thread_count() { return thread_count; }
-
-inline std::vector<std::pair<int, int>> WorldInfo::get_thread_distribution() {
-  std::vector<std::pair<int, int>> distribution;
-  for (auto node_info : nodes) {
-    distribution.push_back(
-        {node_info->get_rank(), node_info->get_thread_count()});
-  }
-  return distribution;
+  MPI_Datatype mpi_node_type;
+  MPI_Type_create_struct(3, blocklengths, displacements, types, &mpi_node_type);
+  MPI_Type_commit(&mpi_node_type);
+  return mpi_node_type;
 }
 
 inline MPICluster::MPICluster() {
   MPI_Init(NULL, NULL);
-  self = std::make_shared<NodeInfo>();
-  world = std::make_shared<WorldInfo>();
+  MPI_Comm_size(MPI_COMM_WORLD, &node_count);
 
-  MPI_Datatype mpi_node_info_type = self->get_mpi_type();
+  self = std::make_shared<Node>();
+
+  MPI_Datatype mpi_node_type = self->get_mpi_type();
 
   if (self->is_master()) {
-    world->add_node(self);
-    for (int i = 1; i < world->get_node_count(); ++i) {
-      NodeInfo node_info;
-      MPI_Recv(&node_info, 1, mpi_node_info_type, i, 0, MPI_COMM_WORLD,
+    add_node(self);
+    for (int i = 1; i < node_count; ++i) {
+      Node node;
+      MPI_Recv(&node, 1, mpi_node_type, i, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
-      world->add_node(std::make_shared<NodeInfo>(node_info));
+      add_node(std::make_shared<Node>(node));
     }
-    world->print_info();
+    print_info();
   } else {
-    MPI_Send(self.get(), 1, mpi_node_info_type, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(self.get(), 1, mpi_node_type, 0, 0, MPI_COMM_WORLD);
   }
 }
 
 inline bool MPICluster::on_master() { return self->is_master(); }
 
-inline int MPICluster::get_node_count() { return world->get_node_count(); }
+inline int MPICluster::get_node_count() { return node_count; }
 
-inline int MPICluster::get_total_thread_count() { return world->get_thread_count(); }
+inline int MPICluster::get_total_thread_count() { return thread_count; }
 
-inline int MPICluster::get_local_thread_count() { return self->get_thread_count(); }
+inline int MPICluster::get_local_thread_count() {
+  return self->get_thread_count();
+}
 
 inline int MPICluster::get_rank() { return self->get_rank(); }
 
+inline void MPICluster::add_node(std::shared_ptr<Node> node_ptr) {
+  thread_count += node_ptr->get_thread_count();
+  nodes.push_back(node_ptr);
+
+  if (!node_ptr->is_linux()) {
+    os_linux = false;
+  }
+}
+
 inline std::vector<std::pair<int, int>> MPICluster::get_thread_distribution() {
-  return world->get_thread_distribution();
+  std::vector<std::pair<int, int>> distribution;
+  for (auto node : nodes) {
+    distribution.push_back({node->get_rank(), node->get_thread_count()});
+  }
+  return distribution;
+}
+
+inline bool MPICluster::is_linux() { return os_linux; }
+
+inline void MPICluster::print_info() {
+  for (auto node_info : nodes) {
+    node_info->print_info();
+  }
 }
 
 inline MPICluster::~MPICluster() { MPI_Finalize(); }
