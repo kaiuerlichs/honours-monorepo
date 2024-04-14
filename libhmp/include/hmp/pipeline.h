fd92f5f6d4cb9e2b6613a261d31d897efa711727
Kai Uerlichs <kai@uerlichs.de>
2024-04-14 01:13:59 +0100

    refactor pipeline implementation

--- libhmp/include/hmp/pipeline.h
+++ libhmp/include/hmp/pipeline.h
@@ -264,6 +264,7 @@ void Pipeline<IN_TYPE, OUT_TYPE>::allocate_stages() {
 template <typename IN_TYPE, typename OUT_TYPE>
 void Pipeline<IN_TYPE, OUT_TYPE>::run_stages(std::vector<IN_TYPE> &data) {
   std::cout << allocation.self << std::endl;
+  std::cout << data.size() << std::endl;
   stages[allocation.self]->run_self(cluster, allocation, data);
 }
 
